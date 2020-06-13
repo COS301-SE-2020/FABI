1b39ecde945881d72f27791bc5c19c1f0a86f588
Zandolor <steven.j.jordaan2000@gmail.com>
2020-06-13 21:02:32 +0200

    Migrate register to GraphQL
    Added the new functionality for GraphQL registering with custom error reporting

--- Frontend/src/app/_services/user.service.ts
+++ Frontend/src/app/_services/user.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import {environment} from '@/environment';
 import { User } from '@/_models/user';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor() { }
+    constructor(private apollo: Apollo) { }
 
     register(user: User) {
-        
+        return this.apollo.mutate({
+            mutation: gql`mutation {
+                registerUser( request: {
+                    name: "${user.firstName}", surname: "${user.lastName}", email: "${user.username}", password: "${user.password}", userType: "basic"
+                })
+                {
+                    token,
+                    status
+                }
+            }`
+        }).pipe(map(user => {
+            if (user["data"]["registerUser"]["status"] == "201"){
+                return user;
+            } else {
+                throw new Error(`Error status code ${user["data"]["registerUser"]["status"]}`);
+            }
+
+        }))
     }
 
 }
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -18,18 +18,5 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.loadAllUsers();
-    }
-
-    deleteUser(id: number) {
-        this.userService.delete(id)
-            .pipe(first())
-            .subscribe(() => this.loadAllUsers());
-    }
-
-    private loadAllUsers() {
-        this.userService.getAll()
-            .pipe(first())
-            .subscribe(users => this.users = users);
     }
 }
