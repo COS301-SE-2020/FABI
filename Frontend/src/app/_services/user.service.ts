f4102f4974a77fb0c1966ac8a9d4c7e3a4f79894
Zandolor <steven.j.jordaan2000@gmail.com>
2020-06-13 20:31:46 +0200

    Remove REST register
    Removed the old register functionality to make way for the GraphQL mutation and also removed other test REST functions

--- Frontend/src/app/_services/user.service.ts
+++ Frontend/src/app/_services/user.service.ts
@@ -6,17 +6,10 @@ import { User } from '@/_models/user';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor(private http: HttpClient) { }
-
-    getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users`);
-    }
+    constructor() { }
 
     register(user: User) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
+        
     }
 
-    delete(id: number) {
-        return this.http.delete(`${environment.apiUrl}/users/${id}`);
-    }
 }
