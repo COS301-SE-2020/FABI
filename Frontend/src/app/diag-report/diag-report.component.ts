178b0e1ed5f4d96b576b38e26e783d16542b5bfb
Bradez-of-map-n-stone <u16063458@tuks.co.za>
2020-06-13 20:49:04 +0200

    Plant and Disease questions
    Added plant and disease/pest questions with ngIf for the last question that only shows depending on the number of plants infected

--- Frontend/src/app/diag-report/diag-report.component.ts
+++ Frontend/src/app/diag-report/diag-report.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-diag-report',
@@ -10,6 +9,10 @@ import { DatePipe } from '@angular/common';
 export class DiagReportComponent implements OnInit {
   diagForm: FormGroup;
   today : number = Date.now();
+  Q1=[];
+  Q4 = [];
+  Q6=[];
+  selectedValue: string = '';
 
   constructor(private formBuilder: FormBuilder) {
    }
@@ -17,8 +20,45 @@ export class DiagReportComponent implements OnInit {
   ngOnInit(): void {
     this.diagForm = this.formBuilder.group({
       Date: ['', Validators.required],
-      Location: ['', Validators.required]
+      Location: ['', Validators.required],
+      CN: ['', Validators.required],
+      SN: [''],
+      Cu: [''],
+      Q1: [''],
+      Q2: [''],
+      Q3: [''],
+      Q4: [''],
+      Q5: [''],
+      Q6:['']
   });
-  }
+  this.Q1 = this.getPlantParts();
+  this.Q4 = this.getnumPlants();
+  this.Q6 = this.getYNConditional();
+  
+
+
+}
+
+getnumPlants() {
+  return [
+    { id: 'np1', name: 'One' },
+    { id: 'np2', name: 'More than one' }
+  ];
+}
+getPlantParts(){
+  return [
+    {id: 'pp1', name: 'Root'},
+    {id: 'pp2', name: 'Stem'},
+    {id: 'pp3', name: 'Branch'},
+    {id: 'pp4', name: 'Leaf / Leaves'},
+    {id: 'pp5', name: 'Flowers'}
+  ];
+}
+getYNConditional(){
+  return [
+    {id:"1", name:"Yes"},
+    {id:"2", name:"No"}
+  ];
+}
 
 }
