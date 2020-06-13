c0e08309320192d2fd36ea13c9856f4d506c9423
Bradez-of-map-n-stone <u16063458@tuks.co.za>
2020-06-13 18:51:11 +0200

    add diagReport page and form
    Added a link to the diagReport page from home which contains a basic form

--- Frontend/src/app/app.routing.ts
+++ Frontend/src/app/app.routing.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_helpers/auth.guard';
 import { from } from 'rxjs';
+import { DiagReportComponent } from './diag-report/diag-report.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'DiagReport', component: DiagReportComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
--- Frontend/src/app/diag-report/diag-report.component.ts
+++ Frontend/src/app/diag-report/diag-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-diag-report',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diag-report.component.css']
 })
 export class DiagReportComponent implements OnInit {
+  diagForm: FormGroup;
+  today : number = Date.now();
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+   }
 
   ngOnInit(): void {
+    this.diagForm = this.formBuilder.group({
+      Date: ['', Validators.required],
+      Location: ['', Validators.required]
+  });
   }
 
 }
