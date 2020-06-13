152062b86659680ee0a2a202c11ca7b9b6e7f8e7
Zandolor <steven.j.jordaan2000@gmail.com>
2020-06-13 22:51:49 +0200

    Fix spec provider issue
    The spec files needed Apollo to be imported as a provider before they would work

--- Frontend/src/app/_services/authentication.service.ts
+++ Frontend/src/app/_services/authentication.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -13,7 +12,7 @@ export class AuthenticationService {
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
 
-    constructor(private http: HttpClient, private apollo: Apollo) {
+    constructor(private apollo: Apollo) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
     }
