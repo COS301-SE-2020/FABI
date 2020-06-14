3ceff92036fbdba09554844341739756ae72681f
Zandolor <steven.j.jordaan2000@gmail.com>
2020-06-14 02:36:14 +0200

    Move photo capture
    Moved the photo capture functionality to the diagnostic report to be sent along with form data

--- Frontend/src/app/diag-report/diag-report.component.ts
+++ Frontend/src/app/diag-report/diag-report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
+import {Subject, Observable} from 'rxjs';
 
 import { LocationService } from '@/_services/location.service';
 
@@ -22,8 +24,29 @@ export class DiagReportComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private location : LocationService) {
     
    }
+  // toggle webcam on/off
+  public showWebcam = true;
+  public allowCameraSwitch = true;
+  public multipleWebcamsAvailable = false;
+  public deviceId: string;
+  public videoOptions: MediaTrackConstraints = {
+    // width: {ideal: 1024},
+    // height: {ideal: 576}
+  };
+  public errors: WebcamInitError[] = [];
 
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
+
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+  // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
+  private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
   ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs()
+    .then((mediaDevices: MediaDeviceInfo[]) => {
+      this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+    });
     this.location.getLocation().subscribe(rep=>{
       this.lat=rep.coords.latitude;
       this.long=rep.coords.longitude;
@@ -50,6 +73,43 @@ export class DiagReportComponent implements OnInit {
   
 
 
+}
+
+public triggerSnapshot(): void {
+  this.trigger.next();
+}
+
+public toggleWebcam(): void {
+  this.showWebcam = !this.showWebcam;
+}
+
+public handleInitError(error: WebcamInitError): void {
+  this.errors.push(error);
+}
+
+public showNextWebcam(directionOrDeviceId: boolean|string): void {
+  // true => move forward through devices
+  // false => move backwards through devices
+  // string => move to device with given deviceId
+  this.nextWebcam.next(directionOrDeviceId);
+}
+
+public handleImage(webcamImage: WebcamImage): void {
+  console.info('received webcam image', webcamImage);
+  this.webcamImage = webcamImage;
+}
+
+public cameraWasSwitched(deviceId: string): void {
+  console.log('active device: ' + deviceId);
+  this.deviceId = deviceId;
+}
+
+public get triggerObservable(): Observable<void> {
+  return this.trigger.asObservable();
+}
+
+public get nextWebcamObservable(): Observable<boolean|string> {
+  return this.nextWebcam.asObservable();
 }
 
 getnumPlants() {
