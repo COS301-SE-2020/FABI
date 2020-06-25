175b883baa0021421e1ebe93f566541163642089
DanieNel21 <u18028277@tuks.co.za>
2020-06-25 15:10:27 +0200

    Added get by report ID

--- API/fabi-api/src/database/Report/report.service.ts
+++ API/fabi-api/src/database/Report/report.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import  Reports  from './report.entity';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+
+    constructor(
+        @InjectRepository(Reports)
+        private ReportsRepository: Repository<Reports>
+    ){}
+
+    async getReportbyID(id : string): Promise<Reports>{
+        const report = await this.ReportsRepository.findOne({reportID:id});
+        return report;
+    }
+}
