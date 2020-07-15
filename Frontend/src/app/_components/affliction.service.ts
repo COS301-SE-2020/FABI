68b72c8717a71e8db3dbe52aa8ab5773886b38b3
Zandolor <steven.j.jordaan2000@gmail.com>
2020-07-15 16:32:58 +0200

    Add mock affliction service
    Added a dummy service to work on the UI until api connection is possible

---
+++ Frontend/src/app/_components/affliction.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import * as afflictionsDS from '../PestAndDiseases.json'
+import { Affliction } from '../_models/affliction'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AfflictionService {
+
+  afflictions: Affliction[]
+
+  constructor() { }
+
+  getPests() {
+    return [
+      {
+        "ScienceName": "Pissodes nemorensis",
+        "Name": "Deodar weevil",
+        "Plant": "Pinus species",
+        "Type": "Pest",
+        "Distribution": "Throughout South Africa",
+        "Status": "Pest populations are high, but don’t always cause serious damage. The exception is in stands of P. radiata where feeding on the tips can result in tip die-back.",
+        "Description": "Adults have long, curved snouts and are reddish brown in colour with two patches of light grey scales on their backs. The body length of the adults ranges from 6-8mm. The larvae are yellowish white, cylindrical and legless, with light brown heads, and they are about 6mm long when fully grown.",
+        "Symptoms": "Dying or dead pine shoots, often resulting in forking or branching of trees. Circular emergence holes on the bark of the main stem, which DO NOT penetrate the wood. These exit holes lead from the pupal chambers or ‘chip cocoons’ between the bark and the wood.",
+        "Management": "Remove dying and dead trees, as these provide host material for the population of the beetle to increase. A native parasitoid has been detected parasitising the larvae.",
+        "Images": [
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" },
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" },
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" }
+        ]
+
+      },
+      {
+        "ScienceName": "Thaumastocoris peregrinus",
+        "Name": "Bronze bug",
+        "Plant": "Eucalyptus species and hybrids",
+        "Type": "Pest",
+        "Distribution": "Throughout South Africa",
+        "Status": "Populations vary through the year",
+        "Description": "Adults are small (2-4mm), light-brown sapsucking insects. Eggs are small, oval and black, and can be laid singly or in clusters.",
+        "Symptoms": "Initial reddening of the canopy leaves which become reddish-yellow or yellow-brown, coupled with some leaf loss and the visible abundance of adults, nymphs and black egg capsules in clusters. During severe infestations, loss of leaves leads to canopy thinning and branch dieback.",
+        "Management": "An egg parasitoid wasp, Cleruchoides noackae (Mymaridae) has been released as a biological control agent, with the first releases taking place in 2013.",
+        "Images": [
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" },
+          { "Image": "https://bicep.net.au/wp-content/uploads/2019/03/T.-peregrinus.jpg" }
+        ]
+
+      }
+    ]
+  }
+
+  getPathogens() {
+    return [
+      {
+        "ScienceName": "Puccinia psidii",
+        "Name": "rust pathogen",
+        "Species": "Affects Eucalypts and native Myrtaceae",
+        "Type": "Disease",
+        "Distribution": "Currently known from KZN south coast, Gauteng and Limpopo (New Agatha, Wolkberg) on native Myrtaceae",
+        "Status": "",
+        "Description": "",
+        "Symptoms": "Leaf spots and death of young new shoots. These are often covered by bright yellow spore masses. Biology: Requires high humidity and periods of low light for germination and infection. Mostly a problem in sub-tropical areas of the world.",
+        "Management": "Report immediately to TPCP and destroy plants once disease has been confirmed. Selection breeding is possible to manage the disease. Do not move infected plants",
+        "Images": [
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" },
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" },
+          { "Image": "https://extension.msstate.edu/sites/default/files/publication-images/P3057/DEOD2.png" }
+        ]
+
+      }
+    ]
+  }
+}
