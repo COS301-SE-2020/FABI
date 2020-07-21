67e6e9af4591f1eecaac4820299e47a68282a66a
Zandolor <steven.j.jordaan2000@gmail.com>
2020-07-21 09:45:56 +0200

    Add pathogen admin functionality
    Added the typescript functionality for the pathogen list

--- Frontend/src/app/_components/pathogen-info/pathogen-info.component.ts
+++ Frontend/src/app/_components/pathogen-info/pathogen-info.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfflictionService } from '../affliction.service'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'apollo-link';
+import { Affliction } from '@/_models/affliction';
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-pathogen-info',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pathogen-info.component.css']
 })
 export class PathogenInfoComponent implements OnInit {
+  displayedColumns: string[] = ["Scientific Name", "Common Name", "Affected Plant", "Actions"]
+  pests
+  dataSource
+  constructor(private afflictionService: AfflictionService, private router: Router) { }
 
-  constructor() { }
+  @ViewChild(MatSort, { static: true }) sort: MatSort
 
   ngOnInit(): void {
+    this.pests = this.afflictionService.getPests()
+    this.dataSource = new MatTableDataSource(this.pests)
+    this.dataSource.sort = this.sort
+  }
+
+  updatePathogen(id: number) {
+    this.router.navigateByUrl(`admin/updatePathogen/${id}`)
   }
 
 }
