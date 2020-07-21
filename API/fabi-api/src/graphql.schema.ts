02f81f01b6e7ccad0c445b4046184ff8b4a831fd
Shaun Naude <shaunnaude20@gmail.com>
2020-07-21 12:00:54 +0200

    Schema created

--- API/fabi-api/src/graphql.schema.ts
+++ API/fabi-api/src/graphql.schema.ts
@@ -30,6 +30,23 @@ export class Request {
     password: string;
 }
 
+export class Update_afflictions_request {
+    id: number;
+    token: string;
+    type?: string;
+    scienceName?: string;
+    name?: string;
+    plant?: string;
+    distribution?: string;
+    status?: string;
+    description?: string;
+    symptoms?: string;
+    management?: string;
+    img1?: string;
+    img2?: string;
+    img3?: string;
+}
+
 export class UploadRequest {
     token: string;
     report: string;
@@ -62,6 +79,8 @@ export class LoginResponse {
 export abstract class IMutation {
     abstract get_afflictions(request?: All_afflictions_request): All_afflictions[] | Promise<All_afflictions[]>;
 
+    abstract update_afflictions(request?: Update_afflictions_request): Update_afflictions_response | Promise<Update_afflictions_response>;
+
     abstract login(request?: LoginRequest): LoginResponse | Promise<LoginResponse>;
 
     abstract registerUser(request?: Request): Response | Promise<Response>;
@@ -78,6 +97,10 @@ export abstract class IQuery {
 export class Response {
     status: number;
     token: string;
+}
+
+export class Update_afflictions_response {
+    status?: number;
 }
 
 export class UploadResponse {
