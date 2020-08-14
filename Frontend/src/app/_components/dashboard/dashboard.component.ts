414585b108dcf5f0a25b64bd61114abc4d0d0ddd
Zandolor <steven.j.jordaan2000@gmail.com>
2020-08-14 10:44:28 +0200

    Add reverted dashboard component functionality

--- Frontend/src/app/_components/dashboard/dashboard.component.ts
+++ Frontend/src/app/_components/dashboard/dashboard.component.ts
@@ -11,6 +11,9 @@ import { MatPaginator } from '@angular/material/paginator';
 export class DashboardComponent implements OnInit {
 
   cards = []
+  cardPercentage = []
+  cardTotal = []
+  cardLabel = []
   pieChart = []
   pageSizeOptions = []
   displayedColumns: string[] = ['scienceName', 'name', 'management', 'count'];
@@ -21,8 +24,20 @@ export class DashboardComponent implements OnInit {
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
-    this.cards = this.dashboardService.cards()
-    this.pieChart = this.dashboardService.pieChart()
+    this.dashboardService.cards().subscribe(data => {
+      for (let obj in data) {
+          this.cardLabel.push(data[obj]["name"])
+          this.cardTotal.push(data[obj]["thisWeek"])
+          this.cards.push(
+            [ data[obj]["twoWeeksAgo"], data[obj]["lastWeek"], data[obj]["thisWeek"] ]
+          )
+          let divisor = data[obj]["lastWeek"] + data[obj]["twoWeeksAgo"]
+          this.cardPercentage.push(
+            (divisor > 0) ? (data[obj]["thisWeek"] / divisor) : 0
+          )
+      }
+    })
+
     this.dashboardService.adminTable().subscribe(data => {
       data = JSON.parse(data)
       this.dataSource = data
