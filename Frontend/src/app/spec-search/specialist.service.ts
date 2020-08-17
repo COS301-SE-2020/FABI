3b0fabcd5be198956f651214f54cc319f580b467
SJ-Jordaan <steven.j.jordaan2000@gmail.com>
2020-08-17 20:50:09 +0200

    Add specialist service imports

--- Frontend/src/app/spec-search/specialist.service.ts
+++ Frontend/src/app/spec-search/specialist.service.ts
@@ -1,5 +1,10 @@
+// Angular specific imports
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
+// API imports
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 @Injectable({
   providedIn: 'root'
 })
