1806644c3f92961573b6d1505baa3834b941a266
Shaun Naude <shaunnaude20@gmail.com>
2020-08-18 00:01:31 +0200

    add endpoint to check UserType
    This endpoint will be used within the User Management System to check the userType

--- API/fabi-api/src/app.module.ts
+++ API/fabi-api/src/app.module.ts
@@ -1,15 +1,16 @@
 import { LoginRegisterModule } from './login-register/login-register.module';
 import { Module } from '@nestjs/common';
-import {GraphQLModule} from '@nestjs/graphql'
+import { GraphQLModule } from '@nestjs/graphql'
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import * as Joi from '@hapi/joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UploadModule } from './report/upload/upload.module';
-import Users from  "./database/Users/Users.entity"
+import Users from "./database/Users/Users.entity"
 import { AfflictionRequestsModule } from './afflictionInfo/affliction-requests.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { PopulateTableModule } from './report/populate-table/populate-table.module';
+import { AuthUserModule } from './auth-user/auth-user.module';
 
 
 
@@ -17,19 +18,21 @@ import { PopulateTableModule } from './report/populate-table/populate-table.modu
   imports: [
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
-      resolverValidationOptions:{
+      resolverValidationOptions: {
         requireResolversForResolveType: false,
       },
       installSubscriptionHandlers: true,
-      
+
     }),
-        LoginRegisterModule,
-        DatabaseModule,
-        UploadModule,
-        AfflictionRequestsModule,
-        DashboardModule,
-        PopulateTableModule,
-       ],
+    LoginRegisterModule,
+    DatabaseModule,
+    UploadModule,
+    AfflictionRequestsModule,
+    DashboardModule,
+    PopulateTableModule,
+    AuthUserModule,
+    AuthUserModule
+  ],
 
 })
-export class AppModule {}
+export class AppModule { }
