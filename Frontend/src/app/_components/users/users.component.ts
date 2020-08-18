3702f207179bb007ae4c40625037b726ef1a5877
SJ-Jordaan <steven.j.jordaan2000@gmail.com>
2020-08-18 12:49:28 +0200

    Add user service

--- Frontend/src/app/_components/users/users.component.ts
+++ Frontend/src/app/_components/users/users.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfflictionService } from '../affliction.service'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'apollo-link';
+import { Affliction } from '@/_models/affliction';
+import { Router, ActivatedRoute } from '@angular/router'
+import { UserService } from '@/_services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +13,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ["email", "name", "surname", "actions"]
+  users
+  dataSource
+  
+  constructor(private userService: UserService) { }
+  
+  @ViewChild(MatSort, {static: true}) sort: MatSort
 
   ngOnInit(): void {
+
   }
 
 }
