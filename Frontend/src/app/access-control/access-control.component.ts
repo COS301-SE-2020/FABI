cb4b3006e85424ce52f6df4b1952285fd643f7b4
Bradez-of-map-n-stone <u16063458@tuks.co.za>
2020-08-19 03:05:46 +0200

    Forbidden reroute

--- Frontend/src/app/access-control/access-control.component.ts
+++ Frontend/src/app/access-control/access-control.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '@/_UMservices/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-access-control',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./access-control.component.css']
 })
 export class AccessControlComponent implements OnInit {
+  userType="";
 
-  constructor() { }
+  constructor(
+    private auth:AuthenticationService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.auth.getUserType(this.auth.currentUserValue).subscribe(data=>{
+      this.userType=data;
+    });
+  }
+
+  returnTo(){
+    this.auth.getUserType(this.auth.currentUserValue).subscribe(data=>{
+      this.router.navigate(["/"+data]);    
+    });
   }
 
 }
