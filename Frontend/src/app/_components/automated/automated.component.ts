7d348b774dac73751dc9f34d4cf53b7319284a1a
SJ-Jordaan <steven.j.jordaan2000@gmail.com>
2020-08-19 04:56:36 +0200

    Add automated mock data generation

--- Frontend/src/app/_components/automated/automated.component.ts
+++ Frontend/src/app/_components/automated/automated.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// Highchart specific imports
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+import { DashboardService } from '@/_components/dashboard.service';
+import { Options } from "highcharts";
 
 @Component({
   selector: 'app-automated',
@@ -6,10 +11,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./automated.component.css']
 })
 export class AutomatedComponent implements OnInit {
-
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Options;
   constructor() { }
 
   ngOnInit(): void {
+    this.chartOptions = {
+      chart: {
+        type: 'spline',
+        animation: Highcharts.svg, // don't animate in old IE
+        marginRight: 10,
+        events: {
+          load: function () {
+            // set up the updating of the chart each second
+            var series = this.series[0];
+            setInterval(function () {
+              var x = (new Date()).getTime(), // current time
+                y = Math.random();
+              series.addPoint([x, y], true, true);
+            }, 1000);
+          }
+        }
+      },
+      time: {
+        useUTC: false
+      },
+      title: {
+        text: 'Live random data'
+      },
+      accessibility: {
+        announceNewData: {
+          enabled: true,
+          minAnnounceInterval: 15000,
+          announcementFormatter: function (allSeries, newSeries, newPoint) {
+            if (newPoint) {
+              return 'New point added. Value: ' + newPoint.y;
+            }
+            return false;
+          }
+        }
+      },
+      xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150
+      },
+      yAxis: {
+        title: {
+          text: 'Value'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
+      },
+      tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
+      },
+      legend: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false
+      },
+      series: [{
+        type: 'spline',
+        name: 'Random data',
+        data: (function () {
+          // generate an array of random data
+          var data = [],
+            time = (new Date()).getTime(),
+            i;
+
+          for (i = -19; i <= 0; i += 1) {
+            data.push({
+              x: time + i * 1000,
+              y: Math.random()
+            });
+          }
+          return data;
+        }())
+      }]
+    }
   }
 
 }
